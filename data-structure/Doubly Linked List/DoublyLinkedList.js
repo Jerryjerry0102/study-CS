d957a7bd240669eab3a2554593f51de6f9e6a05e
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-05-17 21:06:30 +0900

    Doubly Linked List - push, pop, shift, unshift method

--- data-structure/Doubly Linked List/DoublyLinkedList.js
+++ data-structure/Doubly Linked List/DoublyLinkedList.js
@@ -12,4 +12,65 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  // Pushing: Adding a node to the end of the Doubly Linked List
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  // Popping: Removing a node from the end of the Doubly Linked List
+  pop() {
+    if (!this.head) return undefined;
+    const poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null; // 의사코드에는 설명 없음
+    }
+    this.length--;
+    return poppedNode;
+  }
+  // Shifting: Removing a node from the beginning of the Doubly Linked List
+  shift() {
+    if (this.length === 0) return undefined;
+    const oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+  // Unshifting: Adding a node to the beginning of the Doubly Linked List
+  unshift(val) {
+    const newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
+
+const list = new DoublyLinkedList();
+console.log(list);
