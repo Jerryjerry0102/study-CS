46bcb2c8571433693a031667a22c3fa7a56e5f63
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-05-20 23:48:53 +0900

    Binary Search Trees - insert, find method

--- data-structure/BinarySearchTree/BinarySearchTree.js
+++ data-structure/BinarySearchTree/BinarySearchTree.js
@@ -10,6 +10,37 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(val) {
+    const node = new Node(val);
+    if (this.root === null) {
+      this.root = node;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (val < current.val) {
+        if (current.left === null) {
+          current.left = node;
+          return this;
+        } else current = current.left;
+      } else if (val > current.val) {
+        if (current.right === null) {
+          current.right = node;
+          return this;
+        } else current = current.right;
+      } else return undefined;
+    }
+  }
+  find(val) {
+    if (this.root === null) return undefined;
+    let current = this.root;
+    while (current) {
+      if (val === current.val) return current; // return true
+      if (val < current.val) current = current.left;
+      else if (val > current.val) current = current.right;
+    }
+    return false;
+  }
 }
 
 const tree = new BinarySearchTree();
